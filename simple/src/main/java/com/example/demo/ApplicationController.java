18ade800e0c7e0448d3003391cfaa282f5a98a3b
Parth Swami <60224090+Parth-Swami@users.noreply.github.com>
2020-02-21 21:42:19 +0530

    Add files via upload

---
+++ simple/src/main/java/com/example/demo/ApplicationController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ApplicationController {
+
+	@Autowired
+	UserService userService;
+
+	@RequestMapping("/welcome")
+	public String Welcome(HttpServletRequest request) {
+		request.setAttribute("mode", "MODE_HOME");
+		return "welcomepage";
+	}
+
+	@RequestMapping("/register")
+	public String registration(HttpServletRequest request) {
+		request.setAttribute("mode", "MODE_REGISTER");
+		return "welcomepage";
+	}
+
+	@PostMapping("/save-user")
+	public String registerUser(@ModelAttribute User user, BindingResult bindingResult, HttpServletRequest request) {
+		userService.saveMyUser(user);
+		request.setAttribute("mode", "MODE_HOME");
+		return "welcomepage";
+	}
+
+	@GetMapping("/show-users")
+	public String showAllUsers(HttpServletRequest request) {
+		request.setAttribute("users", userService.showAllUsers());
+		request.setAttribute("mode", "ALL_USERS");
+		return "welcomepage";
+	}
+
+	@RequestMapping("/delete-user")
+	public String deleteUser(@RequestParam int id, HttpServletRequest request) {
+		userService.deleteMyUser(id);
+		request.setAttribute("users", userService.showAllUsers());
+		request.setAttribute("mode", "ALL_USERS");
+		return "welcomepage";
+	}
+}
---
+++ simple/src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@Transactional
+public class UserService {
+	
+	private final UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository=userRepository;
+	}
+	
+	public void saveMyUser(User user ) {
+		userRepository.save(user);
+	}
+	
+	public List<User> showAllUsers(){
+		List<User> users = new ArrayList<User>();
+		for(User user : userRepository.findAll()) {
+			users.add(user);
+		}
+		
+		return users;
+	}
+	
+	public void deleteMyUser(int id) {
+		userRepository.deleteById(id);
+	}
+	public User findByUsernameAndPassword(String username, String password) {
+		return userRepository.findByUsernameAndPassword(username, password);
+	}
+	
+	}
+
+	
